11172717eabe9b0d643d96e1fcd5abbf0b6ddf3a
Filip-Martin Brinkmann <filip.brinkmann@unibas.ch>
2016-03-02 20:11:08 +0100

    changed copying of strings to pointer passing, preventing copying of large strings. added solution hints.

--- advent10/adventofcode10.go
+++ advent10/adventofcode10.go
@@ -28,18 +28,20 @@ import (
 // What is the length of the result?
 
 // Your puzzle input is 1113222113.
+// Correct answers are 252594 (40 iterations) and
+// 3579328 (50 iterations)
 
 var input string = "1113222113"
 
 func main() {
 	// Part one has 40 iterations, part two has 50
-	//iterations := 40
+	// iterations := 40
 	iterations := 50
 	part_one_iterations := 40
 	next_sequence := input
 	for i := 0; i < iterations; i++ {
 		// fmt.Println(next_sequence)
-		fmt.Println("iteration", i)
+		fmt.Println("iteration", i+1)
 		current_sequence := next_sequence
 		next_sequence = ""
 		// iterate over all sequences
@@ -47,15 +49,15 @@ func main() {
 		for current_pointer < len(current_sequence) {
 			// time.Sleep(100 * time.Millisecond)
 			// transform each sequence into string
-			end_of_this_sequence := endOfCurrentSequence(current_sequence, current_pointer)
+			end_of_this_sequence := endOfCurrentSequence(&current_sequence, current_pointer)
 			// fmt.Println(end_of_this_sequence)
-			new_subsequence := transformSequence(current_sequence[current_pointer:end_of_this_sequence])
+			new_subsequence := transformSequence(&current_sequence, current_pointer, end_of_this_sequence)
 			// fmt.Println(new_subsequence)
 			// concatenate strings and add to next_sequence
 			next_sequence += new_subsequence
 			current_pointer = end_of_this_sequence
 		}
-		if i == part_one_iterations || i == iterations-1 {
+		if i == part_one_iterations-1 || i == iterations-1 {
 			fmt.Println("Sequence has length ", len(next_sequence))
 		}
 	}
@@ -63,22 +65,22 @@ func main() {
 
 // Returns the index of the field right of the end of the current sequence. If, e.g., the current sequence
 // has length 1, it returns the current pointer+1.
-func endOfCurrentSequence(current_sequence string, current_pointer int) int {
-	if current_pointer == len(current_sequence)-1 {
+func endOfCurrentSequence(current_sequence *string, current_pointer int) int {
+	if current_pointer == len(*current_sequence)-1 {
 		return current_pointer + 1
 	}
-	sequence_char := current_sequence[current_pointer]
-	for i := current_pointer + 1; i < len(current_sequence); i++ {
-		if sequence_char != current_sequence[i] {
+	sequence_char := (*current_sequence)[current_pointer]
+	for i := current_pointer + 1; i < len(*current_sequence); i++ {
+		if sequence_char != (*current_sequence)[i] {
 			return i
 		}
 	}
-	return len(current_sequence) // same sequence char until end of string
+	return len(*current_sequence) // same sequence char until end of string
 }
 
 // a sequence consists of a number of the same number literals
-func transformSequence(subsequence string) string {
-	character := string(subsequence[0])
-	amount := len(subsequence)
+func transformSequence(sequence *string, begin int, end int) string {
+	character := string((*sequence)[begin])
+	amount := len((*sequence)[begin:end])
 	return strconv.Itoa(amount) + character
 }
